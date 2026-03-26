ce8868e69bde251dd0b30d8b8e58f94b58b9c7c8
Attila0604 <141563166+Attila0604@users.noreply.github.com>
2026-03-26 11:53:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -183,4 +183,4 @@ async def status():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=False)
